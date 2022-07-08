f72dcd27f187077f1670ca1e12bfa08eda6ce060
Aakash Vishwakarma <aakash.vishwakarma@cloudeq.com>
2022-07-08 19:22:09 +0530

    changed python file name

--- main.tf
+++ main.tf
@@ -200,8 +200,8 @@
 # }
 
 locals {
-  lambda_source_file  = "demo.py"
-  lambda-zip-location= "outputs/demo.zip"                                  
+  lambda_source_file  = "Demo.py"
+  lambda-zip-location= "outputs/Demo.zip"                                  
 }
 
 data "archive_file" "lambda_zip" {                                                              
@@ -214,18 +214,11 @@ module "lambda" {
     filename = data.archive_file.lambda_zip.output_path
     function_name    = "Prism-lambda-function"
     description      = "Demo lambda function for Prism POC"
-    handler          = "demo.lambda_handler"
+    handler          = "Demo.lambda_handler"
     source_code_hash = filebase64sha256("${data.archive_file.lambda_zip.output_path}")
 
     create_lambda_layer        = false
     create_eventbridge_trigger = false
-    # event_rule_name            = var.event_rule_name
-    # event_description          = var.event_description
-    # event_schedule_expression  = var.event_schedule_expression
-    # event_statement_id         = var.event_statement_id
-    # event_action               = var.event_action
-    # event_principal            = var.event_principal
-
     tags = merge({"Owner"="aakash"},
         { Function = format("%s", "Prism-lambda-function") }
   )
